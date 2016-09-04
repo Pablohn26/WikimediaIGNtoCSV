e5e581bca4f8623fc21c08c8af08ad288d6c96cd
Pablohn26 <pablohn6@gmail.com>
2016-09-04 23:36:38 +0200

    Add unfinished duplicated tags management

--- get_acta.py
+++ get_acta.py
@@ -6,7 +6,7 @@ from lxml import etree
 import csv
 
 #Archivo XML que cargar
-file = "01/xml/spaignActa_1927_05675720150567572015.xml"
+file = "../datos/ftppriv.cnig.es/01/xml/spaignActa_1927_05675720150567572015.xml"
 #file = "./18/xml/spaignActa_1990_1042142015071042142015.xml"
 #file = sys.argv[1]
 root = etree.parse(file)
@@ -52,15 +52,34 @@ def sanitize_tag(element):
 load_tags(tags_file,tags)
 
 
+#Gestiona las etiquetas duplicadas añadiendo un número al final en caso de coincidencia
+def update_key(dicct,kkey):
+    occurrences = 0
+#    for i in dicct.keys():
+#        if kkey  
+    
+#            if (str(dicct.keys())).find(kkey):
+#                print ("REPE: "+kkey+str(occurrences))
+#                print("occurrences = "+str(occurrences))
+#                ++occurrence
+
+    return kkey+str(occurrences)
+
+
 #Bucle principal que recorre todo el XML imprimiendo la ruta y el contenido de cada elemento 
 for element in root.iter():
     if element.text and not element.text.isspace():
-#        print("%s - %s" % (get_parents(element), element.text))
-        (key, val) = (get_parents(element), element.text)
-        data[str(key)] = val
+        #print("%s - %s" % (get_parents(element), element.text))
+#        print("%s" % (get_parents(element)))
+        (key, val) = (kkey, element.text)
+        if key in data:
+            data[str(update_key(data,key))] = val
+#            print ("DUP: "+key)
+        else: data[str(key)] = val
 
+outputname = file + ".csv"
 
-with open('output2.csv','w') as f:
+with open("salidote.csv",'w') as f:
     w = csv.DictWriter(f, data.keys(),'raise')
     w.writeheader()
     w.writerow(data)
