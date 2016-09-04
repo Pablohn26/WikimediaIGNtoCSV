b254236d3774b12dc843f945bdd3a93f50f0792b
Pablohn26 <pablohn6@gmail.com>
2016-09-05 00:50:47 +0200

    Finishing code. Testing

--- get_acta.py
+++ get_acta.py
@@ -6,7 +6,8 @@ from lxml import etree
 import csv
 
 #Archivo XML que cargar
-file = "../datos/ftppriv.cnig.es/01/xml/spaignActa_1927_05675720150567572015.xml"
+file = "../datos/ftppriv.cnig.es/17/xml/spaignActa_1923_05918020150591802015.xml"
+#file = "../datos/ftppriv.cnig.es/01/xml/spaignActa_1927_05675720150567572015.xml"
 #file = "./18/xml/spaignActa_1990_1042142015071042142015.xml"
 #file = sys.argv[1]
 root = etree.parse(file)
@@ -51,35 +52,34 @@ def sanitize_tag(element):
 #Carga de las etiquetas
 load_tags(tags_file,tags)
 
+#Diccionario para la gestión de etiquetas duplicadas
+dup_tags = {}
 
-#Gestiona las etiquetas duplicadas añadiendo un número al final en caso de coincidencia
+
+#Gestiona las etiquetas duplicadas añadiendo un número al final de la etiqueta en caso de coincidencia
 def update_key(dicct,kkey):
-    occurrences = 0
-#    for i in dicct.keys():
-#        if kkey  
-    
-#            if (str(dicct.keys())).find(kkey):
-#                print ("REPE: "+kkey+str(occurrences))
-#                print("occurrences = "+str(occurrences))
-#                ++occurrence
-
-    return kkey+str(occurrences)
+    if kkey in dup_tags:
+        dup_tags[str(kkey)] = dup_tags[str(kkey)]+1
+    else:
+        dup_tags[str(kkey)] = 1
+    return str(kkey)+str(dup_tags[str(kkey)])
 
 
 #Bucle principal que recorre todo el XML imprimiendo la ruta y el contenido de cada elemento 
 for element in root.iter():
     if element.text and not element.text.isspace():
-        #print("%s - %s" % (get_parents(element), element.text))
+#Imprimir etiqueta y contenido del XML
+#        print("%s - %s" % (get_parents(element), element.text))
+#Imprimir etiqueta del XML
 #        print("%s" % (get_parents(element)))
-        (key, val) = (kkey, element.text)
+        (key, val) = (get_parents(element), element.text)
         if key in data:
             data[str(update_key(data,key))] = val
-#            print ("DUP: "+key)
         else: data[str(key)] = val
 
 outputname = file + ".csv"
 
-with open("salidote.csv",'w') as f:
+with open("prueba1.csv",'w') as f:
     w = csv.DictWriter(f, data.keys(),'raise')
     w.writeheader()
     w.writerow(data)
